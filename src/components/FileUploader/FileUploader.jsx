816efca8b37dfe081ca7e9ed2a0a205761429054
Sunil Brijlall <brijsunil2@gmail.com>
2025-06-18 21:03:35 -0400

    style(FileUploader.scss): working on styles

--- src/components/FileUploader/FileUploader.jsx
+++ src/components/FileUploader/FileUploader.jsx
@@ -1,12 +1,37 @@
-import "./FileUploader.scss";
 import { useState } from "react";
+import "./FileUploader.scss";
 
 const FileUploader = () => {
   const [files, setFiles] = useState([]);
+  const [dragActive, setDragActive] = useState(false);
+
+  const handleFiles = (selectedFiles) => {
+    setFiles((prev) => [...prev, ...Array.from(selectedFiles)]);
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setDragActive(false);
+    if (e.dataTransfer.files && e.dataTransfer.files.length > 0) {
+      handleFiles(e.dataTransfer.files);
+    }
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setDragActive(true);
+  };
+
+  const handleDragLeave = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setDragActive(false);
+  };
 
   const handleFileChange = (e) => {
-    const selectedFiles = Array.from(e.target.files);
-    setFiles((prevFiles) => [...prevFiles, ...selectedFiles]);
+    handleFiles(e.target.files);
   };
 
   const handleCompress = () => {
@@ -14,9 +39,34 @@ const FileUploader = () => {
   };
 
   return (
-    <div className="file-uploader-container">
-      <h2>File Uploader</h2>
-      <input type="file" multiple onChange={handleFileChange} />
+    <div className="file-uploader">
+      <p className="intro">
+        Fast and easy file compression. Upload and zip files right in your
+        browser — no installation needed.
+      </p>
+      <div className="function-btns">
+        <button onClick={handleCompress} disabled={files.length <= 0}>
+          Compress
+        </button>
+      </div>
+      <div
+        className={`drop-zone ${dragActive ? "active" : ""}`}
+        onDragOver={handleDragOver}
+        onDragLeave={handleDragLeave}
+        onDrop={handleDrop}
+      >
+        <p>Drag & Drop files here or click to browse</p>
+        <label for="file-upload" class="custom-file-input-btn">
+          📁 Upload Files
+        </label>
+        <input
+          className="file-input"
+          type="file"
+          multiple
+          onChange={handleFileChange}
+        />
+      </div>
+
       {files.length > 0 && (
         <div className="file-list">
           <ul>
@@ -24,7 +74,6 @@ const FileUploader = () => {
               <li key={idx}>{file.name}</li>
             ))}
           </ul>
-          <button onClick={handleCompress}>Compress</button>
         </div>
       )}
     </div>
