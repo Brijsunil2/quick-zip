7840917386d8adf136650e50c599c62477f645a2
Sunil Brijlall <brijsunil2@gmail.com>
2025-07-30 18:57:16 -0400

    refactor file uploader and add new fileDropZone component

--- src/components/FileUploader/FileUploader.jsx
+++ src/components/FileUploader/FileUploader.jsx
@@ -1,27 +1,16 @@
-import { useState, useEffect } from "react";
-import { FaUpload } from "react-icons/fa";
+import { useState } from "react";
 import { MdOutlineFolderZip } from "react-icons/md";
 import { FaDownload } from "react-icons/fa6";
 import FilesList from "./FilesList";
-import "./FileUploader.scss";
+import FileDropZone from "../FileDropZone/FileDropZone";
 import ProgressBar from "../ProgressBar/ProgressBar";
 import { useZipWorker } from "../util/useZipWorker";
-import { readableFileSize } from "../util/readableFileSize";
+import "./FileUploader.scss";
 
 const FileUploader = () => {
   const [files, setFiles] = useState([]);
   const [dragActive, setDragActive] = useState(false);
-  const {progress, zippedBlob, zipFiles} = useZipWorker();
-  const [totalFileSize, setTotalFileSize] = useState(0);
-
-
-   const getTotalFileSize = () => {
-    return files.reduce((total, file) => total + parseInt(file.size), 0);
-  };
-
-  useEffect(() => {
-    setTotalFileSize(getTotalFileSize());
-  }, [files]);
+  const { progress, zippedBlob, zipFiles } = useZipWorker();
 
   const handleFiles = (selectedFiles) => {
     setFiles((prev) => [...prev, ...Array.from(selectedFiles)]);
@@ -33,27 +22,6 @@ const FileUploader = () => {
 
   const handleRemoveAllFiles = () => {
     setFiles([]);
-  };
-
-  const handleDrop = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    setDragActive(false);
-    if (e.dataTransfer.files && e.dataTransfer.files.length > 0) {
-      handleFiles(e.dataTransfer.files);
-    }
-  };
-
-  const handleDragOver = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    setDragActive(true);
-  };
-
-  const handleDragLeave = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    setDragActive(false);
   };
 
   const handleUpload = (e) => {
@@ -80,6 +48,7 @@ const FileUploader = () => {
         Fast and easy file compression. Upload and zip files right in your
         browser — no installation needed.
       </p>
+
       <div className="function-btns">
         <button
           onClick={handleZip}
@@ -88,10 +57,12 @@ const FileUploader = () => {
           <MdOutlineFolderZip /> Zip Files
         </button>
       </div>
+
       {progress > 0 && progress < 100 && <ProgressBar progress={progress} />}
       {progress === 100 && (
         <p>Zip completed! Click the download button to get your zipped file.</p>
       )}
+
       {zippedBlob && (
         <div className="download-btns">
           <button onClick={handleDownload}>
@@ -102,24 +73,13 @@ const FileUploader = () => {
 
       {(progress === 0 || progress === 100) && (
         <>
-          <div
-            className={`drop-zone ${dragActive ? "active" : ""}`}
-            onDragOver={handleDragOver}
-            onDragLeave={handleDragLeave}
-            onDrop={handleDrop}
-          >
-            <p>Drag & Drop files here or click to browse</p>
-            <label htmlFor="file-upload" className="custom-file-input-btn">
-              <FaUpload /> Upload Files
-            </label>
-            <input
-              id="file-upload"
-              className="file-input"
-              type="file"
-              multiple
-              onChange={handleUpload}
-            />
-          </div>
+          <FileDropZone
+            dragActive={dragActive}
+            setDragActive={setDragActive}
+            handleFiles={handleFiles}
+            handleUpload={handleUpload}
+          />
+
           <FilesList
             files={files}
             handleRemoveFile={handleRemoveFile}
@@ -127,19 +87,6 @@ const FileUploader = () => {
           />
         </>
       )}
-<<<<<<< HEAD
-=======
-      {files.length > 0 && (
-        <p className="total-file-size">
-          Total File Size: {readableFileSize(totalFileSize)}
-        </p>
-      )}
-      <FilesList
-        files={files}
-        handleRemoveFile={handleRemoveFile}
-        handleRemoveAllFiles={handleRemoveAllFiles}
-      />
->>>>>>> bc70d883cc725ae065c79dd45361a38a0a94f9dc
     </div>
   );
 };
